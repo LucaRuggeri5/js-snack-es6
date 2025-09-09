4291d6c42d59d027a058055453ccf3e87d381d67
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2025-09-09 17:40:51 +0200

    fatte le squadre con il generatore di numeri

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -3,3 +3,19 @@
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadre = [
+  { nome: "Peroni", punti: 0, falliSubiti: 0 },
+  { nome: "Ichnusa", punti: 0, falliSubiti: 0 },
+  { nome: "Tennets", punti: 0, falliSubiti: 0 },
+  { nome: "Heineken", punti: 0, falliSubiti: 0 },
+  { nome: "Raffo", punti: 0, falliSubiti: 0 },
+  { nome: "Rolator", punti: 0, falliSubiti: 0 }
+];
+
+
+for (let i in squadre) {
+  squadre[i].punti = Math.floor(Math.random() * 100 + 1);
+  squadre[i].falliSubiti = Math.floor(Math.random() * 500 + 1);
+}
+console.log(squadre);
